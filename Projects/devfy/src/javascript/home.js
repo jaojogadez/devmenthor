ce07c2f5fae650f7fac0c73c0c58c11458e78dd9
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-08-03 17:08:45 -0300

    feat: adiciona uma recolhimento ao sidebar de home.html

--- Projects/devfy/src/javascript/home.js
+++ Projects/devfy/src/javascript/home.js
@@ -11,3 +11,12 @@ function sendToPlayerView() {
 document.querySelector(".btn-logout").addEventListener("click", () => {
   location.href = "../../index.html";
 })
+
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggle-btn");
+const toggleIcon = document.querySelector("#toggle-btn i");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("expand");
+  toggleIcon.textContent = sidebar.classList.contains("expand") ? "keyboard_double_arrow_left" : "keyboard_double_arrow_right";
+});
